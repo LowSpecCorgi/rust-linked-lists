ea89f894655f8f2cf63c0328278f99519665c8bd
Basilicous <59167551+LowSpecCorgi@users.noreply.github.com>
2022-07-11 18:01:40 +0100

    Initial project version

---
+++ src/third.rs
@@ -0,0 +1,66 @@
+// Persistent list
+use std::rc::Rc;
+
+pub struct List<T> {
+    head: Link<T>,
+}
+
+type Link<T> = Option<Rc<Node<T>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List { head: None }
+    }
+
+    pub fn prepend(&self, elem: T) -> List<T> {
+        List { head: Some(Rc::new(Node { 
+            elem: elem, 
+            next: self.head.clone(),
+        }))}
+    }
+
+    pub fn tail(&self) -> List<T> {
+        List {
+            head: self.head.as_ref().and_then(|node| node.next.clone())
+        }
+    }
+
+    pub fn head(&self) -> List<T> {
+        List {
+            head: self.head.as_ref().map(|node| node.clone())
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        assert_eq!(list.pop(), None);
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        list.push(4);
+        list.push(5);
+
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+    }
+}
