9e6e43a5c66f1e4acdc101bfd19def24a1fb8198
Basilicous <haroldthesenpai@gmail.com>
2022-07-12 13:12:09 +0100

    Added basic doubly linked list implementation

--- src/third.rs
+++ src/third.rs
@@ -1,5 +1,5 @@
 // Persistent list
-use std::rc::Rc;
+use std::{rc::Rc, sync::Arc};
 
 pub struct List<T> {
     head: Link<T>,
@@ -10,6 +10,10 @@ type Link<T> = Option<Rc<Node<T>>>;
 struct Node<T> {
     elem: T,
     next: Link<T>,
+}
+
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
 }
 
 impl<T> List<T> {
@@ -32,6 +36,34 @@ impl<T> List<T> {
 
     pub fn head(&self) -> Option<&T> {
         self.head.as_ref().map(|node| &node.elem)
+    }
+
+    pub fn iter(&self) -> Iter<'_, T> {
+        Iter { next: self.head.as_deref() }
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_deref();
+            &node.elem
+        })
+    }
+}
+
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        let mut head = self.head.take();
+        while let Some(node) = head {
+            if let Ok(mut node) = Rc::try_unwrap(node) {
+                head = node.next.take();
+            } else {
+                break;
+            }
+        }
     }
 }
 
@@ -58,5 +90,15 @@ mod test {
 
         let list = list.tail();
         assert_eq!(list.head(), None);
+    }
+
+    #[test]
+    fn iter() {
+        let list = List::new().prepend(1).prepend(2).prepend(3);
+
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
     }
 }
